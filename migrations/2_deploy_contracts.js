df845114a53f18372dae858af0bb36bf70654228
HenricoW <henrico.ashley.White@gmail.com>
2021-02-15 22:06:59 +0200

    set up for Kovan deployment, pvt keys & eth node details in .secrets file, wallet now deploys with 1 gwei

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,5 +4,5 @@ module.exports = async (deployer, _network, accounts) => {
   await deployer.deploy(Wallet, [accounts[0], accounts[1], accounts[2]], 2);
   const wallet = await Wallet.deployed();
   // const val = new BN(web3.utils.toWei(50));
-  await web3.eth.sendTransaction({ from: accounts[0], to: wallet.address, value: 100000 });
+  await web3.eth.sendTransaction({ from: accounts[0], to: wallet.address, value: 1000000 });
 };
